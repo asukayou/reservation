02542feb6f9c7570d95062cef8832bff1e1f6410
asukayou <asukayoui@gmail.com>
2014-12-26 16:09:44 +0900

    設備追加処理まで

--- src/Ay/ReservationBundle/Controller/FacilityController.php
+++ src/Ay/ReservationBundle/Controller/FacilityController.php
@@ -5,8 +5,12 @@ namespace Ay\ReservationBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+use Ay\ReservationBundle\Entity\Facility;
+use Ay\ReservationBundle\Form\Type\FacilityType;
 
 /**
  * @Route("/facility")
@@ -27,10 +31,24 @@ class FacilityController extends Controller {
 
     /**
      * @Route("/create", name="facility_create")
+     * @Method({"GET", "POST"})
      */
     public function createAction(Request $request) {
         // 追加処理
-        return new Response('追加処理');
+        $facility = new Facility();
+        $form = $this->createForm(new FacilityType(), $facility);
+        if ($request->getMethod() == 'GET') {
+            return $this->render('AyReservationBundle:Facility:create.html.twig', array('form' => $form->createView()));
+        }
+        $form->handleRequest($request);
+        if (!$form->isValid()) {
+            return $this->render('AyReservationBundle:Facility:create.html.twig', array('form' => $form->createView()));
+        }
+        $facility->setDeleted(false);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($facility);
+        $em->flush();
+        return $this->redirect($this->generateUrl('facility_index'));
     }
 
     /**
